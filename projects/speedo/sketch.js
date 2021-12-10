5c41134752e7ec0268a3f3ae1c3a2a96a01e2b90
Vincent Giles <13183572+vgiles@users.noreply.github.com>
2021-12-10 20:52:52 +1100

    update image src again

--- projects/speedo/sketch.js
+++ projects/speedo/sketch.js
@@ -61,7 +61,7 @@ function moveDial() {
 }
 
 function preload() {
-  img = loadImage('/projects/assets/speedometer.png');
+  img = loadImage('/projects/speedo/assets/speedometer.png');
 }
 
 function mousePressed() {
